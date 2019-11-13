0f21a3741ef4dc2c54d0045e8b90e82c8758d600
Lucas Legrand <legrand.lucas0@gmail.com>
2019-11-13 14:00:30 +0100

    design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '06 67 88 90 54',
+    category:      'italian'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '09 88 77 53 21',
+    category:        'french'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
